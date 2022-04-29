7b9bc777e154b68d77f350b4085b774f0cdf0a17
Benroberts37 <benrobsemail@gmail.com>
2022-04-29 07:14:48 -0400

    added npm modules, started on middleware

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,20 @@
+const jwt = require('jsonwebtoken')
+
+
 module.exports = (req, res, next) => {
-  next();
+  const token = req.headers.authorization
+  if (token === undefined) {
+    res.status(401).json({message: 'token required'})
+  } else {
+    jwt.verify(token, async (err, decodedToken) => {
+      if (err) {
+        next({status: 401, messsage: 'token invalid'})
+      } else {
+        req.decodedToken = decodedToken
+        next()
+      }
+    })
+  }
   /*
     IMPLEMENT
 
